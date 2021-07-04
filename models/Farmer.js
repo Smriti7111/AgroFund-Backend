20049438a01b21522ba7207de7a1d47121f11b70
subedisagar12 <subedisagar12@gmail.com>
2021-07-04 18:42:00 +0545

    Farmer Schema created

---
+++ models/Farmer.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const farmerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: false,
+  },
+
+  contact: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+
+  rating: {
+    type: String,
+    required: true,
+  },
+
+  walletAddress: {
+    type: String,
+    required: true,
+  },
+  citizenshipNo: {
+    type: String,
+    required: false,
+  },
+
+  panNo: {
+    type: String,
+    required: false,
+  },
+
+  citizenship: {
+    type: String,
+    required: false,
+  },
+
+  pan: {
+    type: String,
+    required: false,
+  },
+
+  isVerified: {
+    type: Boolean,
+  },
+
+  hasProject: {
+    type: Boolean,
+  },
+});
+
+export default mongoose.model("Farmer", farmerSchema);
