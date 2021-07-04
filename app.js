b692fce5d6695ad79848c99b415612aa45d40af2
subedisagar12 <subedisagar12@gmail.com>
2021-07-04 08:44:34 +0545

    Basic setup

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Test");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || "8888";
+
+mongoose.connect(process.env.DATABASE_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let conn = mongoose.connection;
+
+conn.on("open", () => {
+  console.log("Connected to database");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
