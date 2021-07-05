c6846316922d1b2df59e0e89c7a0fede450459a7
subedisagar12 <subedisagar12@gmail.com>
2021-07-05 09:36:53 +0545

    Minor changes

--- models/Farmer.js
+++ models/Farmer.js
@@ -1,68 +1,71 @@
 import mongoose from "mongoose";
 
-const farmerSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
+const farmerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-  email: {
-    type: String,
-    required: false,
-  },
+    email: {
+      type: String,
+      required: false,
+    },
 
-  contact: {
-    type: String,
-    required: true,
-  },
+    contact: {
+      type: String,
+      required: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
 
-  rating: {
-    type: Number,
-    default: 0,
-  },
+    rating: {
+      type: Number,
+      default: 0,
+    },
 
-  walletAddress: {
-    type: String,
-    required: true,
-  },
-  citizenshipNo: {
-    type: String,
-    required: false,
-  },
+    walletAddress: {
+      type: String,
+      required: true,
+    },
+    citizenshipNo: {
+      type: String,
+      required: false,
+    },
 
-  panNo: {
-    type: String,
-    required: false,
-  },
+    panNo: {
+      type: String,
+      required: false,
+    },
 
-  citizenship: {
-    type: String,
-    required: false,
-  },
+    citizenship: {
+      type: String,
+      required: false,
+    },
 
-  pan: {
-    type: String,
-    required: false,
-  },
+    pan: {
+      type: String,
+      required: false,
+    },
 
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
 
-  hasProject: {
-    type: Boolean,
-    default: false,
+    hasProject: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Farmer", farmerSchema);
